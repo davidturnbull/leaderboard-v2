708acadfd06057007b722e9474ee28d36b2a8cdb
realdavidturnbull <hello@meteortips.com>
2015-05-17 21:37:58 +1000

    Chapter 10 - Methods

--- leaderboard3.js
+++ leaderboard3.js
@@ -29,15 +29,15 @@ if(Meteor.isClient){
     },
     'click .increment': function(){
       var selectedPlayer = Session.get('selectedPlayer');
-      PlayersList.update(selectedPlayer, {$inc: {score: 5} });
+      Meteor.call('modifyPlayerScore', selectedPlayer, 5);
     },
     'click .decrement': function(){
       var selectedPlayer = Session.get('selectedPlayer');
-      PlayersList.update(selectedPlayer, {$inc: {score: -5} });
+      Meteor.call('modifyPlayerScore', selectedPlayer, -5);
     },
     'click .remove': function(){
       var selectedPlayer = Session.get('selectedPlayer');
-      PlayersList.remove(selectedPlayer);
+      Meteor.call('removePlayerData', selectedPlayer);
     }
   });
 
@@ -45,12 +45,7 @@ if(Meteor.isClient){
     'submit form': function(event){
       event.preventDefault();
       var playerNameVar = event.target.playerName.value;
-      var currentUserId = Meteor.userId();
-      PlayersList.insert({
-        name: playerNameVar,
-        score: 0,
-        createdBy: currentUserId
-      });
+      Meteor.call('insertPlayerData', playerNameVar);
     }
   });
 
@@ -61,6 +56,26 @@ if(Meteor.isServer){
   Meteor.publish('thePlayers', function(){
     var currentUserId = this.userId;
     return PlayersList.find({createdBy: currentUserId})
+  });
+
+  Meteor.methods({
+    'insertPlayerData': function(playerNameVar){
+      var currentUserId = Meteor.userId();
+      PlayersList.insert({
+          name: playerNameVar,
+          score: 0,
+          createdBy: currentUserId
+      });
+    },
+    'removePlayerData': function(selectedPlayer){
+      var currentUserId = Meteor.userId();
+      PlayersList.remove({_id: selectedPlayer, createdBy: currentUserId});
+    },
+    'modifyPlayerScore': function(selectedPlayer, scoreValue){
+      var currentUserId = Meteor.userId();
+      PlayersList.update( {_id: selectedPlayer, createdBy: currentUserId},
+                          {$inc: {score: scoreValue} });
+    }
   });
 
 }
