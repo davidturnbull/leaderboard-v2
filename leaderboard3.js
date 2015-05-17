4c9f15740e0a5bec1f70cafc14309f7f627cbebe
realdavidturnbull <hello@meteortips.com>
2015-05-17 21:12:24 +1000

    Chapter 6 - Events

--- leaderboard3.js
+++ leaderboard3.js
@@ -1,11 +1,19 @@
 PlayersList = new Mongo.Collection('players');
 
 if(Meteor.isClient){
+  
   Template.leaderboard.helpers({
     'player': function(){
         return PlayersList.find()
     }
   });
+
+  Template.leaderboard.events({
+    'click .player': function(){
+      console.log("You clicked a .player element");
+    }
+  });
+
 }
 
 if(Meteor.isServer){
