f07b0793211f3c0cab76f13a1ddc115d952a69fb
realdavidturnbull <hello@meteortips.com>
2015-05-17 21:14:01 +1000

    Chapter 7 - Sessions

--- leaderboard3.js
+++ leaderboard3.js
@@ -1,16 +1,24 @@
 PlayersList = new Mongo.Collection('players');
 
 if(Meteor.isClient){
-  
+
   Template.leaderboard.helpers({
     'player': function(){
         return PlayersList.find()
+    },
+    'selectedClass': function(){
+      var playerId = this._id;
+      var selectedPlayer = Session.get('selectedPlayer');
+      if(playerId == selectedPlayer){
+          return "selected"
+      }
     }
   });
 
   Template.leaderboard.events({
     'click .player': function(){
-      console.log("You clicked a .player element");
+      var playerId = this._id;
+      Session.set('selectedPlayer', playerId);
     }
   });
 
