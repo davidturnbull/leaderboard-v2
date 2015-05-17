338575ef95030f8893f96e0eed5e0cf5fcb6a228
realdavidturnbull <hello@meteortips.com>
2015-05-17 21:10:22 +1000

    Chapter 5 - Templates

--- leaderboard3.js
+++ leaderboard3.js
@@ -1,1 +1,13 @@
 PlayersList = new Mongo.Collection('players');
+
+if(Meteor.isClient){
+  Template.leaderboard.helpers({
+    'player': function(){
+        return PlayersList.find()
+    }
+  });
+}
+
+if(Meteor.isServer){
+    // this code only runs on the server
+}
